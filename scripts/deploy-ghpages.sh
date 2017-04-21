0995ae72ba022493dc93d5193a0ac55db5f3ba8e
Long <forallandiyou@gmail.com>
2017-04-21 22:53:08 +0800

    update git email and username

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -4,6 +4,9 @@ set -x -e
 # clone repo
 git clone git@github.com:$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git -b master $CIRCLE_PROJECT_REPONAME
 
+git config --global user.email "longliangyou@gmail.com"
+git config --global user.name "CircleCI"
+
 cd $CIRCLE_PROJECT_REPONAME/webapp
 
 MESSAGE=CircleCI[$CIRCLE_BUILD_NUM]:$CIRCLE_SHA1@$CIRCLE_BRANCH
