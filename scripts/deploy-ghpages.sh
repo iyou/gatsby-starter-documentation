69f12e692123fac67ba7b3ac6610347b7104ca36
Long <forallandiyou@gmail.com>
2017-04-21 23:00:33 +0800

    update gh-pages

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -4,12 +4,13 @@ set -x -e
 # clone repo
 git clone git@github.com:$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git -b master $CIRCLE_PROJECT_REPONAME
 
+# set account identity
 git config --global user.email "longliangyou@gmail.com"
 git config --global user.name "CircleCI"
 
 cd $CIRCLE_PROJECT_REPONAME/webapp
 
-MESSAGE=CircleCI[$CIRCLE_BUILD_NUM]:$CIRCLE_SHA1@$CIRCLE_BRANCH
+MESSAGE="CircleCI[$CIRCLE_BUILD_NUM]: $CIRCLE_SHA1@$CIRCLE_BRANCH [ci skip]"
 
 npm install
 COMMIT_MESSAGE=$MESSAGE npm run deploy
