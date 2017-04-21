fcb117d282e46656a37ed085afa4d93db3d00f86
Long <forallandiyou@gmail.com>
2017-04-21 21:21:02 +0800

    simplify script

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 set -x -e
-git clone git@github.com:$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git -b master knotel-docs
-cd knotel-docs/webapp
+
+# clone repo
+git clone git@github.com:$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git -b master $CIRCLE_PROJECT_REPONAME
+
+cd $CIRCLE_PROJECT_REPONAME/webapp
+
 npm install
-npm install gatsby
-`npm bin`/gatsby build
-cd ../..
-mkdir -p docs
-cp -rf knotel-docs/webapp/public/* docs/
-cd knotel-docs
-git checkout gh-pages
-git rm -rf .
-cp -a ../docs/* .
-git config --global user.email "forallandiyou@gmail.com"
-git config --global user.name "Long"
-git add -A
-git commit -m "Deploy to GitHub pages: $CIRCLE_BUILD_NUM [ci skip]"
-git push --force --quiet origin gh-pages
+npm run deploy
